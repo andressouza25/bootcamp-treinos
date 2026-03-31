96fc4bd0159b2ccd3f2281dc0b7d4b3454272432
Andre Santos <andressouza25@gmail.com>
2026-03-31 19:15:47 -0300

    refactor: add env validation with zod

--- dist/lib/auth.js
+++ dist/lib/auth.js
@@ -1,19 +1,22 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { openAPI } from "better-auth/plugins";
+
+import { env } from "../../src/lib/env.js";
 import { prisma } from "./db.js";
 export const auth = betterAuth({
-    baseURL: process.env.BETTER_AUTH_URL,
-    trustedOrigins: ["http://localhost:3000"],
-    socialProviders: {
-        google: {
-            prompt: "select_account",
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        },
+  baseURL: env.BETTER_AUTH_URL,
+
+  trustedOrigins: [env.WEB_APP_BASE_URL],
+  socialProviders: {
+    google: {
+      prompt: "select_account",
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
     },
-    database: prismaAdapter(prisma, {
-        provider: "postgresql",
-    }),
-    plugins: [openAPI()],
+  },
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  plugins: [openAPI()],
 });
--- dist/lib/db.js
+++ dist/lib/db.js
@@ -1,8 +1,9 @@
 import { PrismaPg } from "@prisma/adapter-pg";
+
+import { env } from "../../src/lib/env.js";
 import { PrismaClient } from "../generated/prisma/client.js";
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = `${env.DATABASE_URL}`;
 const adapter = new PrismaPg({ connectionString });
 const globalForPrisma = global;
 export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
-if (process.env.NODE_ENV !== "production")
-    globalForPrisma.prisma = prisma;
+if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
